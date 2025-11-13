d9e3fa9b1ad6dfc3caa5ba5142ad72e56d5700e4
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-13 09:03:44 +0600

    added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,20 +2,18 @@ import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../Pages/Shared/Footer/Footer";
 import NavBar from "../Pages/Shared/NavBar/NavBar";
 
- 
-
 const Main = () => {
-    const location = useLocation();
-    console.log(location)
-    const noHeaderFooter = location.pathname.includes('login')
-    const noHeaderFooter1 = location.pathname.includes('singUp')
-    return (
-        <div>
-            {noHeaderFooter || noHeaderFooter1 || <NavBar></NavBar>}
-            <Outlet></Outlet>
-            {noHeaderFooter || noHeaderFooter1 || <Footer></Footer>}
-        </div>
-    );
+  const location = useLocation();
+  console.log(location);
+  const noHeaderFooter = location.pathname.includes("login");
+  const noHeaderFooter1 = location.pathname.includes("singUp");
+  return (
+    <div>
+      {noHeaderFooter || noHeaderFooter1 || <NavBar></NavBar>}
+      <Outlet></Outlet>
+      {noHeaderFooter || noHeaderFooter1 || <Footer></Footer>}
+    </div>
+  );
 };
 
 export default Main;
