759e70d726087060b2cfcb833151815d9c400356
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-02-03 00:02:04 +0600

    added

--- src/Pages/Projects/projectsData.js
+++ src/Pages/Projects/projectsData.js
@@ -1,4 +1,13 @@
-import img1 from "../../assets/home/progile-image-2.jpg";
+import Flower from "../../assets/Project-img/Faugei.png";
+import CoffeeStore from "../../assets/Project-img/CoffeStore.jpeg";
+import PH from "../../assets/Project-img/PHTube.png";
+import English from "../../assets/Project-img/English.png";
+import NewGame from "../../assets/Project-img/NewGame.png";
+import Payoo from "../../assets/Project-img/payoo.png";
+import kid from "../../assets/Project-img/Kids.jpg";
+import Auction from "../../assets/Project-img/Auction-Gallery.jpg";
+import Bangladesh from "../../assets/Project-img/Bangladesh.png";
+import Selon from "../../assets/Project-img/Selon.png";
 
 export const projectsData = [
   {
@@ -6,7 +15,7 @@ export const projectsData = [
     title: "Always Fresh Flower",
     description:
       "A fully responsive flower shop landing page built using HTML5 and Tailwind CSS. This project was created as part of an assignment and follows all requirements provided via Figma design.",
-    image: img1,
+    image: Flower,
     topics: ["HTML5", "CSS", "Tailwind CSS"],
     live: "https://faugei.vercel.app",
     code: "https://github.com/mojahidmamu/FAUGEI",
@@ -16,7 +25,7 @@ export const projectsData = [
     title: "Coffee Store",
     description:
       " It's coffee time - Sip & Savor - Relaxation in every sip! Get the nostalgia back! Your companion of every moment! Enjoy the beautiful moments and make them memorable.",
-    image: img1,
+    image: CoffeeStore,
     topics: ["React", "Tailwind CSS", "State", "Component", "Vite"],
     live: "https://coffee-store-by-mojahid.vercel.app/",
     code: "https://github.com/mojahidmamu/COFFEE-STORE",
@@ -26,7 +35,7 @@ export const projectsData = [
     title: "PH Tube",
     description:
       "PH Tube is a web-based video streaming application where users can browse and watch videos sorted by categories such as music, comedy, and tutorials.",
-    image: img1,
+    image: PH,
     topics: ["HMTL", "CSS", "JavaScript"],
     live: "https://ph-tube-gamma.vercel.app/",
     code: "https://github.com/mojahidmamu/PH-TUBE",
@@ -36,7 +45,7 @@ export const projectsData = [
     title: "ENGLISH Janala Project",
     description:
       " “English Janala is an interactive web platform designed to help learners improve their English skills through lessons, exercises, and practice activities, focusing on reading, writing, and grammar.” ",
-    image: img1,
+    image: English,
     topics: [
       "React",
       "Tailwind CSS",
@@ -54,7 +63,7 @@ export const projectsData = [
     title: "New Game",
     description:
       " It's coffee time - Sip & Savor - Relaxation in every sip! Get the nostalgia back! Your companion of every moment! Enjoy the beautiful moments and make them memorable.",
-    image: img1,
+    image: NewGame,
     topics: ["HMTL", "CSS", "JavaScript"],
     live: "https://mojahidmamu.github.io/New-Game/",
     code: "https://github.com/mojahidmamu/New-Game",
@@ -64,7 +73,7 @@ export const projectsData = [
     title: "Payoo NAGAD",
     description:
       " “Payoo Nagad is a digital financial app that allows users to add money, transfer funds, pay bills, and track transaction history with a secure PIN-based system, simulating real-world mobile banking functionality.” ",
-    image: img1,
+    image: Payoo,
     topics: ["HMTL", "CSS", "JavaScript"],
     live: "https://payoo-mfs-xi.vercel.app/",
     code: "https://github.com/mojahidmamu/Payoo-NAGAD",
@@ -74,7 +83,7 @@ export const projectsData = [
     title: "Kids School",
     description:
       " “Empowering students from small age towards vision With the courage, Confidence, Creativity and Compassion to make  their Unique Contribution in a Diverse and Dynamic World.” ",
-    image: img1,
+    image: kid,
     topics: ["HMTL", "CSS"],
     live: "https://payoo-mfs-xi.vercel.app/",
     code: "https://kids-school-ten.vercel.app/",
@@ -84,7 +93,7 @@ export const projectsData = [
     title: "Pillars of “Bangladesh 2.0” ",
     description:
       " Idea , innovation , vision and challenges in New Bangladesh. ",
-    image: img1,
+    image: Bangladesh,
     topics: ["HMTL", "CSS"],
     live: "https://kids-school-ten.vercel.app/",
     code: "hhttps://github.com/mojahidmamu/Bangladesh-2.0",
@@ -94,7 +103,7 @@ export const projectsData = [
     title: "AUCTION Gallery",
     description:
       "Bid in Unique Items from Around the World. Discover rare collectibles, luxury goods and vintage treasures in our curated auctions. ",
-    image: img1,
+    image: Auction,
     topics: ["HMTL", "CSS"],
     live: "https://auction-gallery-project.vercel.app/",
     code: "https://github.com/mojahidmamu/AUCTION-GALLERY",
@@ -104,8 +113,18 @@ export const projectsData = [
     title: "Subro Selon Project",
     description:
       "Bid in Unique Items from Around the World. Discover rare collectibles, luxury goods and vintage treasures in our curated auctions. ",
-    image: img1,
-    topics: ["HMTL", "CSS"],
+    image: Selon,
+    topics: [
+      "HMTL",
+      "CSS",
+      "Ract",
+      "Tailwind CSS",
+      "Component",
+      "Vite",
+      "Login",
+      "Register",
+      "Portfolio"
+    ],
     live: "https://subro-selon.vercel.app/",
     code: "https://github.com/mojahidmamu/subro-selon",
   },
