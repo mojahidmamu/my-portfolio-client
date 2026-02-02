09ebb92b317c98b429df4e1026fa58a605fbfe2d
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-02-02 23:18:00 +0600

    added

--- src/Pages/Projects/projectsData.js
+++ src/Pages/Projects/projectsData.js
@@ -13,6 +13,26 @@ export const projectsData = [
   },
   {
     id: 2,
+    title: "Coffee Store",
+    description:
+      " It's coffee time - Sip & Savor - Relaxation in every sip! Get the nostalgia back! Your companion of every moment! Enjoy the beautiful moments and make them memorable.",
+    image: img1,
+    topics: ["React", "Tailwind CSS", "State", "Component", "Vite"],
+    live: "https://coffee-store-by-mojahid.vercel.app/",
+    code: "https://github.com/mojahidmamu/COFFEE-STORE",
+  },
+  {
+    id: 3,
+    title: "PH Tube",
+    description:
+      "PH Tube is a web-based video streaming application where users can browse and watch videos sorted by categories such as music, comedy, and tutorials.",
+    image: img1,
+    topics: ["HMTL", "CSS", "JavaScript"],
+    live: "https://ph-tube-gamma.vercel.app/",
+    code: "https://github.com/mojahidmamu/PH-TUBE",
+  },
+  {
+    id: 4,
     title: "ENGLISH Janala Project",
     description:
       " “English Janala is an interactive web platform designed to help learners improve their English skills through lessons, exercises, and practice activities, focusing on reading, writing, and grammar.” ",
@@ -30,7 +50,7 @@ export const projectsData = [
     code: "https://github.com/mojahidmamu/ENGLISH-Janala-PROJECT",
   },
   {
-    id: 3,
+    id: 5,
     title: "New Game",
     description:
       " It's coffee time - Sip & Savor - Relaxation in every sip! Get the nostalgia back! Your companion of every moment! Enjoy the beautiful moments and make them memorable.",
@@ -39,5 +59,14 @@ export const projectsData = [
     live: "https://mojahidmamu.github.io/New-Game/",
     code: "https://github.com/mojahidmamu/New-Game",
   },
-   
+  {
+    id: 6,
+    title: "Payoo NAGAD",
+    description:
+      " “Payoo Nagad is a digital financial app that allows users to add money, transfer funds, pay bills, and track transaction history with a secure PIN-based system, simulating real-world mobile banking functionality.” ",
+    image: img1,
+    topics: ["HMTL", "CSS", "JavaScript"],
+    live: "https://payoo-mfs-xi.vercel.app/",
+    code: "https://github.com/mojahidmamu/Payoo-NAGAD",
+  },
 ];
