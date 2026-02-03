f91d13e1ca82cf05a0dfeefe3b8f5cdee08415bc
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-02-03 15:29:54 +0600

    added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,13 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB_wXe1duUbbkcD8bWE0KF9VkByse9WgPk",
-  authDomain: "bistro-boss-2aacc.firebaseapp.com",
-  projectId: "bistro-boss-2aacc",
-  storageBucket: "bistro-boss-2aacc.firebasestorage.app",
-  messagingSenderId: "447374236780",
-  appId: "1:447374236780:web:0fd1c2f581720f6537cc49"
+  apiKey: "AIzaSyDrdH0Bd3I2hHQF0kWZlX0dD1MEdJXxVTY",
+  authDomain: "my-portfolio-ffa81.firebaseapp.com",
+  projectId: "my-portfolio-ffa81",
+  storageBucket: "my-portfolio-ffa81.firebasestorage.app",
+  messagingSenderId: "380623767216",
+  appId: "1:380623767216:web:086ccee0cf9d246e801d75"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
