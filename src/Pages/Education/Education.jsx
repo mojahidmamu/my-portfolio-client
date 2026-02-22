a2f6a8cb63f24cc881fe06293ddd7576e37c5f1c
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-02-22 10:48:50 +0600

    added

--- src/Pages/Education/Education.jsx
+++ src/Pages/Education/Education.jsx
@@ -1,25 +1,127 @@
-import React from 'react';
+    import React from "react";
+    import {
+    VerticalTimeline,
+    VerticalTimelineElement,
+    } from "react-vertical-timeline-component";
+    import "react-vertical-timeline-component/style.min.css";
 
-const Education = () => {
+    const Education = () => {
     return (
-        <div className="py-20 px-6 md:px-12 lg:px-20 bg-gray-50 dark:bg-gray-900 min-h-screen">
-            {/* Header */}
-            <div className="max-w-4xl mx-auto text-center space-y-6 mb-16">
-                <h2 className="text-4xl md:text-5xl font-extrabold tracking-tight">
-                <span className="text-slate-900 dark:text-white">Educational </span>
-                <span className="bg-gradient-to-r from-purple-500 via-indigo-500 to-teal-400 bg-clip-text text-transparent">
-                    Qualifications
-                </span>
-                </h2>
+        <div id="education"  className="my-10 px-4 bg-base-200 border-b-4 border-purple-500 transition-colors duration-300">
+        {/* Header */}
+        <div className="max-w-4xl mx-auto text-center space-y-6 mb-4 mt-4">
+            <h2 className="text-4xl md:text-5xl font-extrabold tracking-tight">
+            <span className="text-slate-900 dark:text-white">Educational </span>
+            <span className="bg-gradient-to-r from-purple-500 via-indigo-500 to-teal-400 bg-clip-text text-transparent">
+                Qualification
+            </span>
+            </h2>
 
-                <div className="flex justify-center">
-                    <span className="h-1 w-28 rounded-full bg-gradient-to-r from-purple-500 to-indigo-500"></span>
-                </div>
-
-
+            <div className="flex justify-center">
+            <span className="h-1 w-28 rounded-full bg-gradient-to-r from-purple-500 to-indigo-500"></span>
             </div>
         </div>
-    );
-};
 
-export default Education;
+        <VerticalTimeline>
+            {/* Educational Item 1 */}
+            <VerticalTimelineElement
+            date="2024 - 2027"
+            contentStyle={{ background: "#F3E8FF", color: "#6A0DAD" }}
+            contentArrowStyle={{ borderRight: "7px solid #6A0DAD" }}
+            iconStyle={{ background: "#6A0DAD", color: "#fff" }}
+            icon={
+                <div className="flex justify-center items-center w-full h-full">
+                <img
+                    src="https://img.icons8.com/?size=100&id=wogrNmTvt9qn&format=png"
+                    alt="Bachelor Icon"
+                    className="w-16 h-16 object-contain"
+                />
+                </div>
+            }
+            >
+            <h3 className="text-[#6A0DAD] text-[20px] font-bold">
+                Diploma in Computer Science
+            </h3>
+            <p className="text-[#4B0082] text-[16px] font-semibold mb-4">
+                Bangladesh Sweden Polytechnic Institute
+            </p>
+            <ul className="list-disc ml-5 space-y-2">
+                <li className="text-[#4B0082] text-[14px]">
+                Diploma in Computer Science and Technology<br />
+                Completed in 2027-<span className="underline font-bold">(runnig)</span> with CGPA: ......
+                </li>
+            </ul>
+            </VerticalTimelineElement>
+
+            {/* Educational Item 2 */}
+            <VerticalTimelineElement
+            date="2018 - 2023"
+            contentStyle={{ background: "#E8EAF6", color: "#3F51B5" }}
+            contentArrowStyle={{ borderRight: "7px solid #3F51B5" }}
+            iconStyle={{ background: "#3F51B5", color: "#fff" }}
+            icon={
+                <div className="flex justify-center items-center w-full h-full">
+                <img
+                    src="https://img.icons8.com/?size=100&id=81348&format=png"
+                    alt="HSC Icon"
+                    className="w-16 h-16 object-contain"
+                />
+                </div>
+            }
+            >
+            <h3 className="text-[#3F51B5] text-[20px] font-bold">
+            Secondary Secondary Certificate
+            </h3>
+            <p className="text-[#303F9F] text-[16px] font-semibold mb-4">
+                Illishia Jamila Begum Hish School-[SSC]
+            </p>
+            <ul className="list-disc ml-5 space-y-2 ">
+                <li className="text-[#303F9F] text-[14px] mt-2">
+                SSC in Science  
+                </li>
+                <li className="text-[#01579B] text-[14px]">
+                GPA: 4.83 / 5.00
+                </li>
+                <li className="text-[#01579B] text-[14px]">
+                Completed in 2023
+                </li>
+            </ul>
+            </VerticalTimelineElement>
+
+            {/* Educational Item 3 */}
+            <VerticalTimelineElement
+            date="2013 - 2017"
+            contentStyle={{ background: "#E3F2FD", color: "#0288D1" }}
+            contentArrowStyle={{ borderRight: "7px solid #0288D1" }}
+            iconStyle={{ background: "#0288D1", color: "#fff" }}
+            icon={
+                <div className="flex justify-center items-center w-full h-full">
+                <img
+                    src="https://img.icons8.com/?size=100&id=RWH5eUW9Vr7f&format=png"
+                    alt="SSC Icon"
+                    className="w-16 h-16 object-contain"
+                />
+                </div>
+            }
+            >
+            <h3 className="text-[#0288D1] text-[20px] font-bold">
+                Primary School Certificate-[PSC]
+            </h3>
+            <p className="text-[#01579B] text-[16px] font-semibold mb-4">
+                Khan Shaheb Mokbul Ali Chy. Academy
+            </p>
+            <ul className="list-disc ml-5 space-y-2 mt-2">
+                <li className="text-[#01579B] text-[14px]">
+                GPA: 4.96 / 5.00
+                </li>
+                <li className="text-[#01579B] text-[14px]">
+                Completed in 2017
+                </li>
+            </ul>
+            </VerticalTimelineElement>
+        </VerticalTimeline>
+        </div>
+    );
+    };
+
+    export default Education;
