8c729604d1f08c1d462cefd3be57e212f32ea0f0
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-02-23 14:52:10 +0600

    added

--- src/Pages/FeautureBlog/blogsData.js
+++ src/Pages/FeautureBlog/blogsData.js
@@ -1,8 +1,11 @@
-import blog1 from "../../assets/Blog-image/Ecommerce_pic.png";
-import blog2 from "../../assets/Blog-image/ManageMate.png";
-import blog3 from "../../assets/Blog-image/Volunteer-management.png";
-import blog4 from "../../assets/Blog-image/Digital-wallet.png";
+// import blog1 from "../../assets/Blog-image/Ecommerce_pic.png";
+// import blog2 from "../../assets/Blog-image/ManageMate.png";
+// import blog3 from "../../assets/Blog-image/Volunteer-management.png";
+// import blog4 from "../../assets/Blog-image/Digital-wallet.png";
 import Hackthon from "../../assets/Blog-image/Hackthon.png";
+import Robotics  from "../../assets/Blog-image/Robotics.png";
+import Github  from "../../assets/Blog-image/Github.png";
+import NSDA  from "../../assets/Blog-image/Github.png";
 
 export const blogs = [
   {
@@ -15,18 +18,18 @@ export const blogs = [
     id: 2,
     title: "Robotics Club Champion 2025 in BSPI ",
     excerpt: "Achieved the title of Robotics Club Champion 2025 at Bangladesh Sweden Polytechnic Institute (BSPI) by securing top position in an inter-institution competition held within our institute. This accomplishment highlights my strong problem-solving skills, technical creativity, and ability to work effectively under competitive pressure. The experience enhanced my teamwork, innovation mindset, and practical understanding of robotics and technology-based solutions.",
-    image: blog2,
+    image: Robotics,
   },
   {
     id: 3,
     title: "Github commit history in 2025",
     excerpt: "In 2025, I maintained strong consistency on GitHub, making 3,500+ code contributions while focusing on daily practice, real-world projects, and strengthening core computer science fundamentals. My activity reflects continuous learning, steady improvement, and growth in web development, Data Structures & Algorithms, and problem-solving. More than numbers, this milestone represents discipline, persistence, and my commitment to becoming a skilled software developer through the mindset of Learn. Build. Improve. Repeat. 🚀",
-    image: blog3,
+    image: Github,
   },
   {
     id: 4,
     title: "NSDA ITS-RPL-(Level-3) in-2026",
     excerpt: "Earned RPL (Recognition of Prior Learning) Level-3 Certification on 03.01.2026, validating my practical experience, hands-on technical knowledge, and strong problem-solving abilities. This certification recognizes my competency in applying real-world skills effectively and reflects my commitment to continuous improvement and professional development in the field of technology.",
-    image: blog4,
+    image: NSDA,
   },
 ];
